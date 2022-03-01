2a6919a0a001834b9abf683d5d3c13e64e0f1a26
anpimartinez <kingfunnel.co@gmail.com>
2022-03-01 09:14:47 -0500

    FluentCRM Pro 2.5.6

---
+++ app/Http/Policies/SequencePolicy.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace FluentCampaign\App\Http\Policies;
+
+use FluentCrm\App\Http\Policies\BasePolicy;
+use FluentCrm\Framework\Request\Request;
+
+class SequencePolicy extends BasePolicy
+{
+    /**
+     * Check user permission for any method
+     * @param  \FluentCrm\Framework\Request\Request $request
+     * @return Boolean
+     */
+    public function verifyRequest(Request $request)
+    {
+        if($request->method() == 'GET') {
+            return $this->currentUserCan('fcrm_read_emails');
+        }
+
+        return $this->currentUserCan('fcrm_manage_emails');
+    }
+}
